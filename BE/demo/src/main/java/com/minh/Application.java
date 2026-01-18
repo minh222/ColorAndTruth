d85ae7ad5759fce1b276c90113d9235918b90c53
minh\minh <rongkho01@gmail.com>
2026-01-18 12:26:19 +0700

    - Docker file

--- BE/demo/src/main/java/com/minh/Application.java
+++ BE/demo/src/main/java/com/minh/Application.java
@@ -4,13 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class Application {
 
     public static void main(String[] args) {
-
-        System.out.println("Hello, World!");
-        SpringApplication.run(DemoApplication.class, args
-
+        SpringApplication.run(Application.class, args
         );
     }
 
