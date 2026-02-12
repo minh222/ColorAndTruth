fffed97cac791101e9bcb7d85a712caf80967b40
minh\minh <rongkho01@gmail.com>
2026-02-12 13:47:59 +0700

    - CORS

--- BE/src/main/java/com/minh/config/SpringConfig.java
+++ BE/src/main/java/com/minh/config/SpringConfig.java
@@ -2,6 +2,8 @@ package com.minh.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import java.util.concurrent.Semaphore;
 import static com.minh.config.Config.CORE;
@@ -12,6 +14,19 @@ public class SpringConfig {
     @Bean("spring")
     public Semaphore getSemaphore() {
         return new Semaphore( CORE*2, true);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://colorandtruth1.onrender.com")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
     }
 
 }
